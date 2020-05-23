39e32cac6e1ac624f5d4eaf2f7dee775130d3f7a
Alief Azies <Alief.ahmadazies@gmail.com>
2020-05-23 21:36:38 +0700

    menambahkan code di SplahActivity.kt

--- app/src/main/java/com/ziesapp/splashscreen/Views/SplashActivity.kt
+++ app/src/main/java/com/ziesapp/splashscreen/Views/SplashActivity.kt
@@ -1,13 +1,33 @@
 package com.ziesapp.splashscreen.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.ziesapp.splashscreen.MainActivity
 import com.ziesapp.splashscreen.R
 
 class SplashActivity : AppCompatActivity() {
 
+    //pemesanan variabel
+    private lateinit var mDelayHandler:Handler
+    private var splashTime:Long = 3000
+
+    //Method setelah splash berakhir
+    internal val mRunnable:Runnable = Runnable {
+        val intent = Intent(this@SplashActivity,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //inisialisasi Handler
+        mDelayHandler = Handler()
+
+        //navigasi dengan durasi
+        mDelayHandler.postDelayed(mRunnable,splashTime)
     }
 }
